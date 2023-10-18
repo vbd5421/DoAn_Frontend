117db5f41848c4602655100e683b526c4346ff43
@vdb5421 <duongvb@ecoit.com.vn>
2023-10-18 21:08:50 +0700

    about-us, contact , room-list

---
+++ src/app/modules/home/aboutus-home/about-list/about-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-list',
+  templateUrl: './about-list.component.html',
+  styleUrls: ['./about-list.component.css']
+})
+export class AboutListComponent {
+
+}
--- src/app/modules/home/booking/book-room/book-room.component.ts
+++ src/app/modules/home/booking/book-room/book-room.component.ts
@@ -20,4 +20,20 @@ export class BookRoomComponent {
       }
       return true;
     }
+
+        //mã ACII
+        numberOnlyPhone(event:any): boolean {
+          const charCode = (event.which) ? event.which : event.keyCode;
+          if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
+          }
+          const input = event.target ; // Lấy đối tượng input
+          if (input.value.length >= 10) {
+            // alert('')
+          return false;
+          }
+          return true;
+        }
+    
+
 }
