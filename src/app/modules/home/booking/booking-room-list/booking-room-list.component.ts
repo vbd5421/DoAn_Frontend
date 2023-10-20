1bca6b1f2b0fc1df7a5739e0fbeb0a0823c23f76
@vdb5421 <duongvb@ecoit.com.vn>
2023-10-20 20:46:56 +0700

    add search room

--- src/app/modules/home/booking/booking-room-list/booking-room-list.component.ts
+++ src/app/modules/home/booking/booking-room-list/booking-room-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-booking-room-list',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class BookingRoomListComponent {
 
+  constructor(private router:Router){}
+  //mã ACII
+  numberOnly(event:any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    const input = event.target ; // Lấy đối tượng input
+    if (input.value.length >= 1) {
+      // alert('')
+    return false;
+    }
+    return true;
+  }
+  searchRoom() {
+    this.router.navigate(['/tim-kiem']);
+  }
+  roomDetail(){
+    this.router.navigate(['/chi-tiet-phong'])
+  }
 }
