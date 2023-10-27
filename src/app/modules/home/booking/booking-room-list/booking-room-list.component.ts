5b88da8962dc3889932808ee6fa7c8fa0b8b51b5
@vdb5421 <duongvb@ecoit.com.vn>
2023-10-27 08:29:29 +0700

    update giao dien

--- src/app/modules/home/booking/booking-room-list/booking-room-list.component.ts
+++ src/app/modules/home/booking/booking-room-list/booking-room-list.component.ts
@@ -10,23 +10,14 @@ import { Router } from '@angular/router';
 export class BookingRoomListComponent {
 
   constructor(private router:Router){}
-  //mã ACII
-  numberOnly(event:any): boolean {
-    const charCode = (event.which) ? event.which : event.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-      return false;
-    }
-    const input = event.target ; // Lấy đối tượng input
-    if (input.value.length >= 1) {
-      // alert('')
-    return false;
-    }
-    return true;
-  }
+
   searchRoom() {
     this.router.navigate(['/tim-kiem']);
   }
   roomDetail(){
     this.router.navigate(['/chi-tiet-phong'])
+  }
+  goback():void{
+    window.history.back();
   }
 }
