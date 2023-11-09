d5c82936b01968a9c2480a5650e4a920f60ef567
@vdb5421 <duongvb@ecoit.com.vn>
2023-11-09 15:46:03 +0700

    update code

--- src/app/modules/home/product/product-home/product-home.component.ts
+++ src/app/modules/home/product/product-home/product-home.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-home',
   templateUrl: './product-home.component.html',
-  styleUrls: ['./product-home.component.css']
+  styleUrls: ['./product-home.component.css'],
 })
 export class ProductHomeComponent {
+  arrayLength = [
+    {
+      src:'https://noithatbinhminh.com.vn/wp-content/uploads/2022/08/anh-dep-40.jpg'
+  },
+  {src:'https://i.pinimg.com/564x/e8/1f/67/e81f678d16aa664e01cde4c2f5f97453.jpg'},
+  {src:'https://i.pinimg.com/564x/e8/1f/67/e81f678d16aa664e01cde4c2f5f97453.jpg'},
+  {
+    src:'https://noithatbinhminh.com.vn/wp-content/uploads/2022/08/anh-dep-40.jpg'
+},
+{src:'https://cdn.tgdd.vn/Files/2023/06/19/1534928/2-200623-100943.jpg'}
+];
+
 
 }
