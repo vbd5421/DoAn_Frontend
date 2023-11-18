b74830863ba1b212f79f845d9aa18f2c46be9832
@vdb5421 <duongvb@ecoit.com.vn>
2023-11-18 10:24:39 +0700

    đưa dl ra home

--- src/app/modules/home/product/product-home/product-home.component.ts
+++ src/app/modules/home/product/product-home/product-home.component.ts
@@ -1,5 +1,11 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { Constant } from 'src/app/core/config/constant';
+import { Domain } from 'src/app/core/domain/domain';
+import { Product } from 'src/app/core/model/product/product';
+import { AuthService } from 'src/app/service/auth/auth.service';
+import { ProductService } from 'src/app/service/product/product.service';
 
 @Component({
   selector: 'app-product-home',
@@ -25,6 +31,53 @@ onWindowScroll(event: Event) {
   } else {
     this.animationState = 'inactive';
   }
+}
+
+products: Product[] = [];
+totalPages: number;
+searchInput = '';
+baseURL = Constant.BASE_URL;
+productURL = Domain.PRODUCT;
+paging = {
+  page: 1,
+  size: 5,
+  totalRecord: 0
+}
+constructor(private router: Router,
+  private auth: AuthService, 
+  private productService: ProductService) {
+}
+ngOnInit(): void {
+  this.getProductListAllwithPage();
+}
+getRequestParams(page: number, pageSize: number, search: string): any {
+  let params: any = {};
+
+  if (page) {
+    params[`pageNo`] = page;
+  }
+
+  if (pageSize) {
+    params[`pageSize`] = pageSize;
+  }
+
+  if (search) {
+    params[`name`] = search;
+  }
+  return params;
+}
+getProductListAllwithPage(){
+  const params = this.getRequestParams(this.paging.page, this.paging.size, this.searchInput)
+  this.productService.getListAllPage(params).subscribe(data => {
+    this.products = data.content;
+    this.paging.totalRecord = data.totalElements;
+    this.totalPages = data.totalPages;
+    console.log('sản phẩm home' , this.products)
+  },
+    error => {
+      console.log(error);
+    }
+  )
 }
 
 }
